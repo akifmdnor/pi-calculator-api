0e4c407ec2bcb79c0dede1f3d31dbf31c69612d3
Akif md nor <dev.akifmdnor@gmail.com>
2021-07-04 19:52:58 +0800

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = 5000;
 const db = require('./db/pi');
 const piCalculator = require('./piCalculator');
 
